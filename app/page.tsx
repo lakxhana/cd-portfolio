6948f8312dd7a7f1bb769a2d290aae053d03e231
LAKXHANA SELVA RAJAH <38977549+lakxhana@users.noreply.github.com>
2025-06-10 14:41:59 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -203,7 +203,7 @@ export default function Home() {
                     </div>
                     <div>
                       <p className="font-medium text-white">Email</p>
-                      <p className="text-gray-300">lakxhanaselavarajah@gmail.com</p>
+                      <p className="text-gray-300">lakxhanaselvarajah@gmail.com</p>
                     </div>
                   </div>
 
